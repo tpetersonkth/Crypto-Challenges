09243b3c40409bbcfdf82f9415f955fea6cc90eb
Thomas Peterson <thpeter@kth.se>
2019-03-10 11:20:30 +0100

    Working implementation

--- AES/AES.cpp
+++ AES/AES.cpp
@@ -16,15 +16,20 @@
 // https://en.wikipedia.org/wiki/Rijndael_S-box
 // https://en.wikipedia.org/wiki/Advanced_Encryption_Standard
 // https://en.wikipedia.org/wiki/Rijndael_key_schedule
+//Testing: https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Standards-and-Guidelines/documents/examples/AES_Core128.pdf
 
 using namespace std;
+
+void cipher(BLOCK in, uint8_t out[4*Nb], uint32_t word[Nb*(Nr+1)]);
 
 void keyExpansion(uint8_t *key, uint32_t *w);
 uint32_t subWord(uint32_t word);
 uint32_t rotWord(uint32_t word);
 
 void addRoundKey(uint32_t *key, uint8_t state[][4]);
-void subBytes(BLOCK block);
+void subBytes(uint8_t state[][4]);
+void shiftRows(uint8_t state[STATE_DIM][STATE_DIM]);
+void mixColumns(uint8_t state[STATE_DIM][STATE_DIM]);
 
 void printByte(uint8_t byte);
 void printWord(uint32_t byte);
@@ -33,8 +38,6 @@ void printBlock(BLOCK block);
 void printState(uint8_t state[][4]);
 
 int main(){
-  uint8_t state[STATE_DIM][STATE_DIM];
-  memset(state, 0, sizeof state);
 
   // Get key
   uint8_t key[16];
@@ -52,9 +55,16 @@ int main(){
   BLOCK block;
   uint8_t byte;
 
+  cout << "empty block" << endl;
+  printBlock(block);
+
   while(!cin.eof()){
     byte = cin.get();
-    block.set(row,col,byte);
+
+    printWord(byte);
+    cout << endl;
+
+    block.set(row,col,(uint8_t)byte);
 
     if (col == 3 && row == 3){
       blocklist.push_back(block);
@@ -71,32 +81,85 @@ int main(){
       }
     }
   }
+  cout << "pushing empty block" << endl;
+  printBlock(block);
   blocklist.push_back(block);
-
-
-  cout << "size:" << blocklist.size() << endl;
-
-  //Copy first block to state
-  for(int r = 0; r < STATE_DIM; r++){
-    for(int c = 0; c < STATE_DIM; c++){
-      state[r][c] = blocklist[0].get(r,c);
-    }
-  }
-
-
-  //blocklist.erase(blocklist.begin());
 
   uint32_t keys[Nb*(Nr+1)];
   keyExpansion(key,keys);
 
-  cout << "state" << flush << endl;
-  printState(state);
 
-  addRoundKey(keys,state);
+  cout << "Starting encryption" << endl;
+  for(int b = 0; b < blocklist.size(); b++){
+    uint8_t out[4*Nb];
+    cipher(blocklist[b], out, keys);
+    printBlock(blocklist[b]);
+    printByteArray(out,4*Nb);
+  }
 
-  printState(state);
+
+
+  //blocklist.erase(blocklist.begin());
 
   return 0;
+}
+
+void cipher(BLOCK in, uint8_t out[4*Nb], uint32_t word[Nb*(Nr+1)]){
+  uint8_t state[STATE_DIM][STATE_DIM];
+  memset(state, 0, sizeof state);
+
+  //Copy first block to state
+  for(int r = 0; r < STATE_DIM; r++){
+    for(int c = 0; c < STATE_DIM; c++){
+     state[r][c] = in.get(r,c);
+    }
+  }
+
+  //cout << "Addroundkey:" << endl;
+  addRoundKey(word,state);
+  //printState(state);
+
+  for(int round = 1; round < Nr; round++){
+    //cout << "round " << dec << round << ", FIGHT!" << endl;
+    //cout << "subBytes:" << endl;
+    subBytes(state);
+    //printState(state);
+
+    //cout << "shiftRows:" << endl;
+    shiftRows(state);
+    //printState(state);
+
+    //cout << "mixColumns:" << endl;
+    mixColumns(state);
+    //printState(state);
+
+    //cout << "Addroundkey:" << endl;
+    addRoundKey(word+Nb*round,state);
+    //printState(state);
+  }
+
+  //cout << "subBytes:" << endl;
+  subBytes(state);
+  //printState(state);
+
+  //cout << "shiftRows:" << endl;
+  shiftRows(state);
+  //printState(state);
+
+  //cout << "Addroundkey:" << endl;
+  addRoundKey(word+Nb*Nr,state);
+  //printState(state);
+
+
+
+  //Extract state into out
+  for(int r = 0; r < STATE_DIM; r++){
+    for(int c = 0; c < STATE_DIM; c++){
+      out[STATE_DIM*c+r] = state[r][c];
+    }
+  }
+
+
 }
 
 //-----Key schedule-----
@@ -147,7 +210,6 @@ uint32_t rotWord(uint32_t word){
 
 
 //-----State manipulation-----
-
 void addRoundKey(uint32_t *key, uint8_t state[][4]){
   for(int c = 0; c < STATE_DIM; c++){
     state[0][c] ^= (key[c] & 0xff000000) >> 24;
@@ -157,27 +219,53 @@ void addRoundKey(uint32_t *key, uint8_t state[][4]){
   }
 }
 
+void subBytes(uint8_t state[STATE_DIM][STATE_DIM]){
+  for(int r = 0; r < STATE_DIM; r++){
+    for(int c = 0; c < STATE_DIM; c++){
+      state[r][c] = sbox[state[r][c]];
+    }
+  }
+}
 
-void subBytes(BLOCK block){
-  for(int r; r < STATE_DIM; r++){
-    for(int c; c < STATE_DIM; c++){
-      block.set(r,c, sbox[block.get(r,c)]);
+void shiftRows(uint8_t state[STATE_DIM][STATE_DIM]){
+  for(int r = 0; r < STATE_DIM; r++){
+    uint8_t temp[STATE_DIM];
+    for(int c = 0; c < STATE_DIM; c++){
+      temp[c] = state[r][(c+r) % Nb];
+    }
+
+    for(int c = 0; c < STATE_DIM; c++) {
+      state[r][c] = temp[c];
+    }
+  }
+}
+
+void mixColumns(uint8_t state[STATE_DIM][STATE_DIM]){
+  for(int c = 0; c < STATE_DIM; c++){
+    uint8_t temp[STATE_DIM];
+    temp[0] = GF2[state[0][c]] ^ GF3[state[1][c]] ^ state[2][c] ^ state[3][c];
+    temp[1] = state[0][c] ^ GF2[state[1][c]] ^ GF3[state[2][c]] ^ state[3][c];
+    temp[2] = state[0][c] ^ state[1][c] ^ GF2[state[2][c]] ^ GF3[state[3][c]];
+    temp[3] = GF3[state[0][c]] ^ state[1][c] ^ state[2][c] ^ GF2[state[3][c]];
+
+    for(int r = 0; r < STATE_DIM; r++) {
+      state[r][c] = temp[r];
     }
   }
 }
 
 //-----Debug-----
 void printByte(uint8_t byte){
-  cout << hex << setfill('0') << setw(2) << static_cast<unsigned>(byte) << endl;
+  cout << hex << setfill('0') << setw(2) << uppercase << static_cast<unsigned>(byte);
 }
 
 void printWord(uint32_t word){
-  cout << hex << setfill('0') << setw(8) << static_cast<unsigned>(word) << endl;
+  cout << hex << setfill('0') << setw(8) << uppercase << static_cast<unsigned>(word) << endl;
 }
 
 void printByteArray(uint8_t *array, int size){
   for(int i = 0; i < size; i++){
-    cout << hex << setfill('0') << setw(2) << static_cast<unsigned>(array[i]);
+    cout << hex << setfill('0') << setw(2) << uppercase << static_cast<unsigned>(array[i]);
   }
   cout << endl;
 }
@@ -185,7 +273,7 @@ void printByteArray(uint8_t *array, int size){
 void printBlock(BLOCK block){
   for(int r = 0; r < STATE_DIM; r++) {
     for (int c = 0; c < STATE_DIM; c++) {
-      cout << hex << setfill('0') << setw(2) << static_cast<unsigned>(block.get(r,c));
+      cout << hex << setfill('0') << setw(2) << uppercase << static_cast<unsigned>(block.get(r,c));
     }
     cout << endl;
   }
@@ -195,7 +283,7 @@ void printBlock(BLOCK block){
 void printState(uint8_t state[][4]){
   for(int r = 0; r < STATE_DIM; r++) {
     for (int c = 0; c < STATE_DIM; c++) {
-      cout << hex << setfill('0') << setw(2) << static_cast<unsigned>(state[r][c]);
+      cout << hex << setfill('0') << setw(2) << uppercase << static_cast<unsigned>(state[r][c]);
     }
     cout << endl;
   }
