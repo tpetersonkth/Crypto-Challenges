a93d762e79a82af3ff3805e7cd8c50b144da7416
Thomas Peterson <thpeter@kth.se>
2019-03-10 17:33:43 +0100

    Cleaned up version that passes kattis

--- modExp/modExp.cpp
+++ modExp/modExp.cpp
@@ -1,4 +1,3 @@
-
 /*
  * Modular Exponentitation
  * Author: Thomas Peterson
@@ -6,88 +5,27 @@
 
 #include <iostream>
 #include <gmpxx.h>
-#include <string>
 
 using namespace std;
 
-uint64_t modexpNaive(uint64_t a, uint64_t e, uint64_t n);
-uint64_t binaryExp(uint64_t x, uint64_t n, uint64_t m);
-long long binExpNoRec(long long b, long long e, long long m);
-
-int main2(){
-
-  string a;
-  string e;
-  string n;
-
-  while(cin >> a){
-    cin >> e;
-    cin >> n;
-
-
-    cout << a << endl;
-    cout << e << endl;
-    cout << n << endl;
-
-
-
-    //uint64_t r = modexpNaive(a,e,n);
-    //uint64_t r = binaryExp(a,e,n) % n;
-    //long long r = binExpNoRec(a,e,n);
-
-    //cout << r << endl;
-
-  }
-
-}
+mpz_class binExpNoRec(mpz_class b, mpz_class e, mpz_class m);
 
 int main(){
 
-  uint64_t a;
-  uint64_t e;
-  uint64_t n;
+  mpz_class a, e, n;
 
   while(cin >> a){
     cin >> e;
     cin >> n;
 
-    //uint64_t r = modexpNaive(a,e,n);
-    //uint64_t r = binaryExp(a,e,n) % n;
-    long long r = binExpNoRec(a,e,n);
-
-    cout << r << endl;
-
+    cout << binExpNoRec(a,e,n) << endl;
   }
 
 }
 
-uint64_t modexpNaive(uint64_t a, uint64_t e, uint64_t n){//Way to slow ;P as expected.. :P
-  uint64_t r = a;
-  while (e > 1){
-    r *= a;
-    e--;
-    r = r % n;
-  }
-  return r;
-}
+mpz_class binExpNoRec(mpz_class b, mpz_class e, mpz_class m){
 
-//https://simple.wikipedia.org/wiki/Exponentiation_by_squaring
-uint64_t binaryExp(uint64_t x, uint64_t n, uint64_t m){
-  if (n == 1){
-    return x;
-  }
-
-  if ((n&0x1) == 0){//n is even
-    return binaryExp(x*x,n/2,m);
-  }
-  else{
-    return (x * binaryExp(x*x,(n-1)/2,m));
-  }
-}
-
-long long binExpNoRec(long long b, long long e, long long m){
-
-  long long r = 1;
+  mpz_class r = 1;
   while (e > 0){
     if (e % 2 == 1){
       r = (r* b) % m;
@@ -98,9 +36,4 @@ long long binExpNoRec(long long b, long long e, long long m){
   }
 
   return r;
-}
-
-void test(){
-  uint64_t r;
-  uint64_t r2;
 }
