2a9d37a3495b7e1ce94992c07ba2a64b184796ef
Thomas Peterson <thpeter@kth.se>
2019-03-14 10:24:03 +0100

    init commit for elpoints

---
+++ ellipticPoints/ellPoints.cpp
@@ -0,0 +1,19 @@
+// Calculation of elliptic points
+// Author: Thomas Peterson
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  uint32_t p,a,b;
+  while(cin >> p){
+    cin >> a;
+    cin >> b;
+
+    cout << "Loaded: p:" << p << " a:" << a << " b:" << b << endl;
+  }
+}
+
