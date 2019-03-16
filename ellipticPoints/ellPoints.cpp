2457c8954647ad670cc354390f9984dd214285d8
Thomas Peterson <thpeter@kth.se>
2019-03-16 11:24:59 +0100

    Passing kattis

--- ellipticPoints/ellPoints.cpp
+++ ellipticPoints/ellPoints.cpp
@@ -1,66 +1,59 @@
 // Calculation of elliptic points
 // Author: Thomas Peterson
-//
 
-#include <iostream>
-#include <math.h>
+#include <iostream> /* cin and cout */
+#include <gmpxx.h> /* mpz_class */
 
 using namespace std;
-bool isSingular(uint64_t a, uint64_t b, uint64_t p);
-bool isQuadResidue(uint64_t z, uint64_t p);
-uint64_t getNumPoints(uint64_t a, uint64_t b, uint64_t p);
+int isSingular(mpz_class a, mpz_class b, mpz_class p);
+bool isQuadResidue(mpz_class z, mpz_class p);
+mpz_class getNumPoints(mpz_class a, mpz_class b, mpz_class p);
 
 int main(){
 
-  uint64_t p,a,b;
+  mpz_class p,a,b;
   while(cin >> p){
     cin >> a;
     cin >> b;
 
-    //cout << "Loaded: p:" << p << " a:" << a << " b:" << b << endl;
-
-    if (isSingular(a,b,p)){
-      cout << 1;
-    }
-    else{
-      cout << 0;
-    }
-    cout << " " << getNumPoints(a,b,p) << endl;
+    cout << isSingular(a,b,p) << " " << getNumPoints(a,b,p) << endl;
 
   }
 }
 
-bool isSingular(uint64_t a, uint64_t b, uint64_t p){
-  uint64_t temp = 4*pow(a,3) + 27*pow(b,2);
-  temp = temp % p;
-  if (temp == 0){
-    return true;
-  }
-  else{
-    return false;
-  }
+int isSingular(mpz_class a, mpz_class b, mpz_class p){
+  //Calculate 4*a^3 + 27*b^2 and check if equal to 0
+  mpz_class exp1 = 3;
+  mpz_class exp2 = 2;
+  mpz_class t1,t2;
+  mpz_powm(t1.get_mpz_t(), a.get_mpz_t(),exp1.get_mpz_t(),p.get_mpz_t());
+  mpz_powm(t2.get_mpz_t(), b.get_mpz_t(),exp2.get_mpz_t(),p.get_mpz_t());
+
+  mpz_class temp = (4*t1 + 27*t2) % p;
+  return temp == 0 ? 1 : 0;
+
 }
 
-bool isQuadResidue(uint64_t z, uint64_t p){
-
-  cout << "p:" << p <<" z:" << z << endl;
-  uint64_t temp = pow(z,(p-1)/2);
-  temp = temp % p;
-  //cout << "p:" << p <<" temp:" << temp << endl;
+bool isQuadResidue(mpz_class z, mpz_class p){
+  //Uses euclidean criterion
+  //z^(p-1)/2 mod p
+  mpz_class temp;
+  mpz_class exp = (p-1)/2;
+  mpz_powm(temp.get_mpz_t(),z.get_mpz_t(),exp.get_mpz_t(),p.get_mpz_t());
 
   return temp == 1;
 }
 
 //https://en.wikipedia.org/wiki/Counting_points_on_elliptic_curves
-uint64_t getNumPoints(uint64_t a, uint64_t b, uint64_t p){
-  uint64_t points = 0;
+mpz_class getNumPoints(mpz_class a, mpz_class b, mpz_class p){
+  mpz_class points = 0;
 
-  for(int x = 0; x < p; x++){
-    uint64_t temp = pow(x,3) + a*x + b;
-    cout << x << "^3" << "+" << a << "*" << x << "+" << b << "=" << temp << endl;
-    temp = temp % p;
-    cout << temp << " mod " << p << endl;
-    //cout << "temp2: " << temp << endl;
+  for(mpz_class x = 0; x < p; x++){
+    mpz_class temp;
+    mpz_class exp = 3;
+    mpz_powm(temp.get_mpz_t(), x.get_mpz_t(),exp.get_mpz_t(),p.get_mpz_t());
+    temp = (temp + a*x + b) % p;
+
     if (isQuadResidue(temp, p)){
       points = points + 2;
     }
