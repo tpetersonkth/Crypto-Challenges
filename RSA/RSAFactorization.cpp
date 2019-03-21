33d5eb00af9dcfe9f09a84325750154c835dcea1
Thomas Peterson <thpeter@kth.se>
2019-03-21 18:33:44 +0100

    Documenation and clean up for RSA factorization

--- RSA/RSAFactorization.cpp
+++ RSA/RSAFactorization.cpp
@@ -1,8 +1,5 @@
-
-/*
- * Factorization of RSA
- * Author: Thomas Peterson
- */
+//Factorization of RSA
+//Author: Thomas Peterson
 
 #include <iostream>
 #include <iomanip>
@@ -15,7 +12,8 @@
 
 using namespace std;
 
-void recoverPrimeFactors(mpz_class n, mpz_class e, mpz_class d, mpz_class *p, mpz_class *q);
+void recoverPrimeFactors(mpz_class n, mpz_class e, mpz_class d, mpz_class *outp, mpz_class *outq);
+void calculateAns(mpz_class y, mpz_class n, mpz_class *outp, mpz_class *outq);
 void outputAns(mpz_class p, mpz_class q);
 
 int main(){
@@ -25,28 +23,41 @@ int main(){
   mpz_class p = 0;
   mpz_class q = 0;
   while(cin >> n){
+    //Read the input
     cin >> e;
     cin >> d;
 
+    //Recover the prime factors
     recoverPrimeFactors(n, e, d, &p, &q);
+
+    //Output the answer
+    outputAns(p,q);
   }
 
 }
 
+/*
+ * Recovers the primae factors p and q from the private exponenent, public exponent and the product pq
+ * Params:
+ *  n - The product pq
+ *  e - The public exponent
+ *  d - The private exponent
+ *  outp - The location to put the first prime factor
+ *  outq - The location to put the second prime factor
+ *
+ * Pseudo code: Appendix C of https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-56Br1.pdf
+ * Theory: http://www.ams.org/notices/199902/boneh.pdf
+ */
 
-//Theory: //http://www.ams.org/notices/199902/boneh.pdf
-//Good reference: https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-56Br1.pdf
-void recoverPrimeFactors(mpz_class n, mpz_class e, mpz_class d, mpz_class *pout, mpz_class *qout) {
+void recoverPrimeFactors(mpz_class n, mpz_class e, mpz_class d, mpz_class *outp, mpz_class *outq) {
   mpz_class p,q;
   mpz_class k = (d*e)-1;
   mpz_class r,t;
 
-  if((k&0x1) == 1){//k is odd
+  if((k&0x1) == 1){//k must be odd
     cout << "Can not compute primefactors for " << k << endl;
     return;
   }
-
-  mpz_class oldk = k;
 
   t = 0;
   while(true){
@@ -58,28 +69,13 @@ void recoverPrimeFactors(mpz_class n, mpz_class e, mpz_class d, mpz_class *pout,
     }
   }
 
-  //cout << "k = " << oldk << " = 2^" << t << "*" << r << endl;
-
-  //Sanity check
-  mpz_class rop = 0;
-  mpz_class base = 2;
-  mpz_pow_ui(rop.get_mpz_t(),base.get_mpz_t(),t.get_ui());
-  if (oldk !=  rop*r){
-    cout << "Calculations went terribly wrong.." << endl;
-    cout << "k = " << oldk << " != 2^" << t << "*" << r << endl;
-  }
-
-  //cout << "n is " << n << endl;
-  //cout << n <<" < RAND_MAX: " << RAND_MAX << endl;
+  //g will give us the prime factors with high probability over 100 iterations
   for(int i = 1; i < 100; i++){
     bool goToNext = false;
 
-    mpz_class g = rand()%n;
-    //cout << "g:" << g << endl;
+    mpz_class g = rand()%n; //Pseudo random numbers are enough for our purposes
     mpz_class y;
     mpz_powm(y.get_mpz_t(),g.get_mpz_t(),r.get_mpz_t(),n.get_mpz_t());//y = g^r mod n
-    //cout << y << " = " << g << "^" << r << " mod " << n << endl;
-    //cout << "y=" << y << endl;
 
     if (y == 1 || y == n-1){
       continue;
@@ -87,16 +83,12 @@ void recoverPrimeFactors(mpz_class n, mpz_class e, mpz_class d, mpz_class *pout,
 
     mpz_class x;
     for(int j = 1; j <= t-1; j++){
+      //Calculate x = y^2 mod n
       mpz_class temp = 2;
-      mpz_powm(x.get_mpz_t(),y.get_mpz_t(),temp.get_mpz_t(),n.get_mpz_t());//x = y^2 mod n
-      //cout << x << " = " << y << "^" << 2 << " mod " << n << endl;
-      //cout << g << ":" << x << endl;
+      mpz_powm(x.get_mpz_t(),y.get_mpz_t(),temp.get_mpz_t(),n.get_mpz_t());
 
       if (x == 1){
-        y = y-1;
-        p = gcd(y,n);
-        q = n/p;
-        outputAns(p,q);
+        calculateAns(y, n, outp, outq);
         return;
       }
 
@@ -116,33 +108,46 @@ void recoverPrimeFactors(mpz_class n, mpz_class e, mpz_class d, mpz_class *pout,
     mpz_powm(x.get_mpz_t(),y.get_mpz_t(),temp.get_mpz_t(),n.get_mpz_t());//x = y^2 mod n
 
     if (x == 1){
-      y = y-1;
-      p = gcd(y,n);
-      q = n/p;
-      outputAns(p,q);
+      calculateAns(y, n, outp, outq);
       return;
     }
   }
 
-  //If we still haven't found x give y = n-1 a last try
+  //If we still haven't found y such that x=1, consider the corner case y = n-1
   mpz_class y=n-1;
   mpz_class temp = 2;
   mpz_class x;
   mpz_powm(x.get_mpz_t(),y.get_mpz_t(),temp.get_mpz_t(),n.get_mpz_t());//x = y^2 mod n
 
   if (x == 1){
-    y = y-1;
-    p = gcd(y,n);
-    q = n/p;
-    outputAns(p,q);
+    calculateAns(y, n, outp, outq);
     return;
   }
 
   cout << "not found" <<  endl;
 
-
 }
 
+/*
+ * Subfunction for recoverPrimeFactors, calculates the prime factors once x is 1 in recoverPrimeFactors
+ * Params:
+ *  y - Corresponds to y in recoverPrimeFactors
+ *  n - Corresponds to n in recoverPrimeFactors
+ *  outp - The output location for the first prime factor
+ *  outq - THe output location for the second prime factor
+ */
+void calculateAns(mpz_class y, mpz_class n, mpz_class *outp, mpz_class *outq){
+  y = y-1;
+  *outp = gcd(y,n);
+  *outq = n/(*outp);
+}
+
+/*
+ * Outputs the two prime numbers in increasing order
+ * Params:
+ *  p - First prime factor
+ *  q - Second prime factor
+ */
 void outputAns(mpz_class p, mpz_class q){
   if (p < q){
     cout << p << " " << q << endl;
